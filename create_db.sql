c2e7e32e9f6aaf8bc1cf39b976af8b4c47d37ac6
hilmawanyr <39452619+hilmawanyr@users.noreply.github.com>
2020-05-30 20:38:45 +0700

    Create create_db.sql

---
+++ create_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE restful_ci3;
+
+CREATE TABLE medium_rest_ci3.mahasiswa (
+	id INT primary key NOT NULL AUTO_INCREMENT,
+	nim varchar(12) NOT NULL,
+	nama varchar(100) NOT NULL,
+	prodi varchar(100) NOT NULL
+);
